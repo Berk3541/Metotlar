85aff40745020e625fc86e4d69d0e973604b1845
Berk3541 <58194819+Berk3541@users.noreply.github.com>
2021-01-16 23:44:36 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        Console.WriteLine("---------Metotlar---------");
+
+        SepetManager sepetManager = new SepetManager();
+        //Altta sepetManager içindekileri çağırır**
+        
+    }   
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {   
+        //naming convension (isimlendirme kuralı)
+        //syntax
+        public void Ekle()
+        {
+            Console.WriteLine("Sepete eklendi");
+        }
+    }
+}
